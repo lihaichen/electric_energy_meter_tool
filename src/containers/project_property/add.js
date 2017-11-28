370c84243b01325b6e8018e24eb96cce11229d11
lihaichen <li8303@yeah.net>
2017-11-28 21:32:59 +0800

    修改添加

--- src/containers/project_property/add.js
+++ src/containers/project_property/add.js
@@ -5,8 +5,6 @@ import React, {Component, PropTypes} from 'react';
 import PropertyForm from './form';
 import {Modal} from 'antd';
 const prefixCls = 'AddProperty';
-
-
 export default class AddProperty extends Component {
   static propTypes = {
     addHandler: PropTypes.func.isRequired,
